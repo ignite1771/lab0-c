0aa79e8d50f06f8aab89ca3146ea58786972d9bc
ignite1771 <ignite1771@gmail.com>
2020-03-03 09:10:34 +0000

    Add q_new
    check q != NULL to assign NULL to q->head

--- queue.c
+++ queue.c
@@ -12,8 +12,9 @@
 queue_t *q_new()
 {
     queue_t *q = malloc(sizeof(queue_t));
-    /* TODO: What if malloc returned NULL? */
-    q->head = NULL;
+    if (q != NULL) {
+        q->head = NULL;
+    }
     return q;
 }
 
