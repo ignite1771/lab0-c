eb66da0c3ae4856a9753e7b187a5030cf3f7d85a
ignite1771 <ignite1771@gmail.com>
2020-07-12 10:40:48 +0800

    Change merge to be iterative
    Use pointer to pointer

--- queue.c
+++ queue.c
@@ -181,19 +181,27 @@ void q_reverse(queue_t *q)
 
 list_ele_t *merge(list_ele_t *l1, list_ele_t *l2)
 {
-    // merge recursively
-    if (!l2)
-        return l1;
-    if (!l1)
-        return l2;
+    list_ele_t *head = NULL;
+    list_ele_t **tmp = &head;
 
-    if (strnatcmp(l1->value, l2->value) < 0) {
-        l1->next = merge(l1->next, l2);
-        return l1;
-    } else {
-        l2->next = merge(l1, l2->next);
-        return l2;
+    while (l1 && l2) {
+        if (strnatcmp(l1->value, l2->value) < 0) {
+            *tmp = l1;
+            l1 = l1->next;
+        } else {
+            *tmp = l2;
+            l2 = l2->next;
+        }
+        tmp = &((*tmp)->next);
     }
+
+    if (l1) {
+        *tmp = l1;
+    }
+    if (l2) {
+        *tmp = l2;
+    }
+    return head;
 }
 
 list_ele_t *merge_sort_list(list_ele_t *head)
